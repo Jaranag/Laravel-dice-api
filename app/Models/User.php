27b5d644a0c872156c3a3049d2220c369a1b07b3
Joël Arana <joel.aranag@gmail.com>
2023-03-24 12:52:34 +0100

    updated models with the needed hasMany relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'email_verified_at',
+        'updated_at'
     ];
 
     /**
@@ -41,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function diceRolls() {
+        return $this->hasMany(DiceRoll::class);
+    }
 }
