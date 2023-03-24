0db8d5eef2982764282a8f93f38b554aaa2b8b88
Joël Arana <joel.aranag@gmail.com>
2023-03-24 18:53:56 +0100

    added roll function to diceroll controller

--- app/Http/Controllers/DiceRollController.php
+++ app/Http/Controllers/DiceRollController.php
@@ -15,4 +15,30 @@ class DiceRollController extends Controller
         $userRolls = $user->diceRolls;
         return $userRolls;
     }
+
+    public function roll($id)
+    {
+        
+        if (auth()->user()->id == $id) {
+            $newRoll = new DiceRoll;
+        $newRoll->id_user = auth()->user()->id;
+        $newRoll->dice1 = rand(1, 6);
+        $newRoll->dice2 = rand(1, 6);
+        $newRoll->result = $newRoll->dice1 +  $newRoll->dice2;
+        if ($newRoll->result == 7) {
+            $newRoll->was_successful = 'Yes';
+        } else {
+            $newRoll->was_successful = 'No';
+        }
+        $this->updateUser($newRoll);
+        $newRoll->save();
+        return $newRoll;
+        } else {
+            return [
+                'error message' => 'can only roll for yourself',
+                'your id' => auth()->user()->id
+            ];
+        }
+    }
+
 }
