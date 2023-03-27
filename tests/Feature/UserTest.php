83421404a57ca5b9943af906e565383b1616fc21
Jaranag <118216573+Jaranag@users.noreply.github.com>
2023-03-27 10:20:50 +0200

    added login tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,16 +6,24 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+
 class UserTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
+
+     public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
+    }
+
     public function test_createUser(): void
     {
         $user = [
-            'username' => 'testUser',
-            'email' => 'test@gmail.com',
+            'username' => fake()->name(),
+            'email' => fake()->safeEmail(),
             'password' => 'test',
         ];
         $response = $this->postJson(route('players.register'), $user)->assertCreated();
@@ -39,7 +47,7 @@ class UserTest extends TestCase
     public function test_createUserAnonymous(): void
     {
         $user = [
-            'email' => 'tested@gmail.com',
+            'email' => fake()->safeEmail(),
             'password' => 'test',
         ];
         $response = $this->postJson(route('players.register'), $user);
@@ -47,5 +55,43 @@ class UserTest extends TestCase
         $response->assertJson(['user' => [
             'username' => 'Anonymous',
         ]]);
+    }
+
+    public function test_login() {
+        $user = [
+            'email' => fake()->safeEmail(),
+            'password' => 'test',
+        ];
+        $response = $this->postJson(route('players.register'), $user);
+        $response->assertJson(['user' => [
+            'email' => $user['email'],
+        ]]);
+        $response = $this->postJson(route('login'), [
+            'email' => $user['email'],
+            'password' => 'test',
+        ]);
+        $response->assertStatus(201);
+        $response->assertJsonfragment([
+            'email' => $user['email'],
+        ]);
+    }
+
+    public function test_login_not_successful() {
+        $user = [
+            'email' => fake()->safeEmail(),
+            'password' => 'test',
+        ];
+        $response = $this->postJson(route('players.register'), $user);
+        $response->assertJson(['user' => [
+            'email' => $user['email'],
+        ]]);
+        $response = $this->postJson(route('login'), [
+            'email' => 'fake@email.com',
+            'password' => 'nose',
+        ]);
+        $response->assertStatus(401);
+        $response->assertJsonfragment([
+            'message' => 'Invalid credentials',
+        ]);
     }
 }
